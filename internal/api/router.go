d9daa159e703d0b06456bc22c41528e7c1772af3
vakshit <akshitv18@gmail.com>
2022-10-15 03:48:10 +0530

    change routes

--- internal/api/router.go
+++ internal/api/router.go
@@ -7,7 +7,7 @@ import (
 func SetupRoutes(app *fiber.App) {
 	app.Get("/all", getAllQuestions)
 	app.Get("/approved", getApprovedQuestions)
-	app.Post("/post", postQuestion)
+	app.Post("/add", postQuestion)
 	app.Post("/answer", answerQuestion)
 	app.Delete("/delete", deleteQuestion)
 }
